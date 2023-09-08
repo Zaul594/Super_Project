b610f2fa4ee7c52aafda9bb5aee0a5883badc972
Zaul_77 <zaul74945@gmail.com>
2023-09-08 15:45:23 -0500

    init

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+	//"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
